4262fae754b43d04d2caf05edd6e261fe2156044
Kevin <shookke@gmail.com>
2019-02-27 17:01:28 -0500

    .

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_admin import helpers as admin_helpers
-#from flask_login import LoginManager
+from flask_login import LoginManager
 from flask_security import Security, SQLAlchemyUserDatastore
 from flask_bootstrap import Bootstrap
 from flask_admin.contrib.sqla import ModelView
@@ -19,8 +19,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-#login = LoginManager(app)
-#login.login_view = 'login'
+login = LoginManager(app)
+login.login_view = 'login'
 
 bootstrap = Bootstrap(app)
 
